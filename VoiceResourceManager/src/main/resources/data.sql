d86cf709611ae40d5e03d0dd17d964b33c011ed4
babis <babis@DESKTOP-44OTH7H>
2018-09-26 07:59:04 +0200

    dev

--- VoiceResourceManager/src/main/resources/data.sql
+++ VoiceResourceManager/src/main/resources/data.sql
@@ -1,25 +1,25 @@
 insert into softswitch(oid,create_date,last_modified,description, name, status, switch_id)
 values
-(SEQ_OID.nextval,'2018-09-25','2018-09-25','blabla', 'cs2k','ONLINE','1'),
-(SEQ_OID.nextval,'2018-09-25','2018-09-25','blabla', 'ngcp','OFFLINE','2');
+(HIBERNATE_SEQUENCE.nextval,'2018-09-25','2018-09-25','blabla', 'cs2k','ONLINE','1'),
+(HIBERNATE_SEQUENCE.nextval,'2018-09-25','2018-09-25','blabla', 'ngcp','OFFLINE','2');
 
 
 insert into resource
-select seq_oid.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 01','1','FREE', oid
+select HIBERNATE_SEQUENCE.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 01','1','FREE', oid
 from softswitch where switch_id=1;
 
 insert into resource
-select seq_oid.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 02','1','FREE', oid
+select HIBERNATE_SEQUENCE.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 02','1','FREE', oid
 from softswitch where switch_id=1;
 
 insert into resource
-select seq_oid.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 03','1','FREE', oid
+select HIBERNATE_SEQUENCE.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 03','1','FREE', oid
 from softswitch where switch_id=1;
 
 insert into resource
-select seq_oid.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 04','1','FREE', oid
+select HIBERNATE_SEQUENCE.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 04','1','FREE', oid
 from softswitch where switch_id=1;
 
 insert into resource
-select seq_oid.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 05','1','FREE', oid
+select HIBERNATE_SEQUENCE.nextval,'2018-09-25 00:00:00','2018-09-25 00:00:00', 'SS 00 0 00 05','1','FREE', oid
 from softswitch where switch_id=1;
