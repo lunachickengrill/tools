07e09a38caacf79ca1c40e3b49d3c886bde2217b
tschwaiger <tschwaiger@libertyglobal.com>
2018-12-12 12:25:31 +0100

    commit

---
+++ VoiceResourceManager/src/main/java/eu/vrtime/vrm/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package eu.vrtime.vrm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@SuppressWarnings("deprecation")
+	public void configure(AuthenticationManagerBuilder builder) throws Exception {
+		builder.inMemoryAuthentication()
+				.passwordEncoder(new BCryptPasswordEncoder())
+				.withUser("user1").password("secret1").roles("USER").and().withUser("admin1").password("secret1")
+				.roles("USER", "ADMIN");
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/api/rest/**").hasRole("USER").antMatchers("/**")
+				.hasRole("ADMIN").and().csrf().disable().headers().frameOptions().disable();
+	}
+}
