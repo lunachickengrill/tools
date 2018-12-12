ef107710e63f3d0aa666ea162d6c9544f9f1a006
tschwaiger <tschwaiger@libertyglobal.com>
2018-12-12 12:33:13 +0100

    commit

--- VoiceResourceManager/src/main/java/eu/vrtime/vrm/config/SecurityConfig.java
+++ VoiceResourceManager/src/main/java/eu/vrtime/vrm/config/SecurityConfig.java
@@ -13,8 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@SuppressWarnings("deprecation")
 	public void configure(AuthenticationManagerBuilder builder) throws Exception {
-		builder.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("user1")
-				.password("secret1").roles("USER").and().withUser("admin1").password("secret1").roles("USER", "ADMIN");
+		builder.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("user")
+				.password("lgs123").roles("USER").and().withUser("dev").password("lgs123").roles("USER", "ADMIN");
 	}
 
 	protected void configure(HttpSecurity http) throws Exception {
