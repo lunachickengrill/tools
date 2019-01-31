ef95b2d8f858016da63753501dce76f24d0633c2
babis <babis@DESKTOP-44OTH7H>
2019-01-31 13:28:31 +0100

    commit

--- SpringBootWicket/src/main/java/eu/vrtime/springbootwicket/web/HomepageForm.java
+++ SpringBootWicket/src/main/java/eu/vrtime/springbootwicket/web/HomepageForm.java
@@ -1,6 +1,7 @@
 package eu.vrtime.springbootwicket.web;
 
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 
@@ -11,6 +12,16 @@ public class HomepageForm extends Form{
 		
 		CompoundPropertyModel model = new CompoundPropertyModel(Model.of(""));
 		this.setModel(model);
+		
+		this.add(new TextField("firstName"));
+		this.add(new TextField("lastName"));
 	}
+
+	@Override
+	protected void onSubmit() {
+		// TODO Auto-generated method stub
+		super.onSubmit();
+	}
+	
 
 }
