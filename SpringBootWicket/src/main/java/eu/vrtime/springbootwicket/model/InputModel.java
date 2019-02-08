b9814c665d10e81672391e0f0d7401d2c1e5a361
tschwaiger <tschwaiger@libertyglobal.com>
2019-02-08 11:09:46 +0100

    commit

---
+++ SpringBootWicket/src/main/java/eu/vrtime/springbootwicket/model/InputModel.java
@@ -0,0 +1,45 @@
+package eu.vrtime.springbootwicket.model;
+
+import java.io.Serializable;
+
+public class InputModel implements Serializable {
+
+	private String fieldOne;
+	private String fieldTwo;
+	private String fieldThree;
+
+	public InputModel(String fieldOne, String fieldTwo, String fieldThree) {
+		this.fieldOne = fieldOne;
+		this.fieldTwo = fieldTwo;
+		this.fieldThree = fieldThree;
+	}
+
+	public InputModel() {
+
+	}
+
+	public String getFieldOne() {
+		return fieldOne;
+	}
+
+	public void setFieldOne(String fieldOne) {
+		this.fieldOne = fieldOne;
+	}
+
+	public String getFieldTwo() {
+		return fieldTwo;
+	}
+
+	public void setFieldTwo(String fieldTwo) {
+		this.fieldTwo = fieldTwo;
+	}
+
+	public String getFieldThree() {
+		return fieldThree;
+	}
+
+	public void setFieldThree(String fieldThree) {
+		this.fieldThree = fieldThree;
+	}
+
+}
--- SpringBootWicket/src/main/java/eu/vrtime/springbootwicket/web/HomePage.java
+++ SpringBootWicket/src/main/java/eu/vrtime/springbootwicket/web/HomePage.java
@@ -1,28 +1,19 @@
 package eu.vrtime.springbootwicket.web;
 
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
+import org.wicketstuff.annotation.mount.MountPath;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 
-@WicketHomePage
+//@WicketHomePage
+@MountPath("/home")
 public class HomePage extends AbstractBasePage {
 
 	public HomePage() {
 		super();
 		add(new Label("homePageLabel", "home page text"));
-		
-		/**
-		 * Strange label and text in textboxes?!
-		 */
 		add(new HomePageForm("homePageForm"));
 
 	}
-
-
 
 }
