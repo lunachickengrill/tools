f54395d271a5b129b98e573e3528d128fc48b302
babis <babis@DESKTOP-44OTH7H>
2019-02-11 11:08:45 +0100

    commit

--- BootWicketWebApp/src/main/java/eu/vrtime/BootWicketWebApp/web/AbstractBasePage.java
+++ BootWicketWebApp/src/main/java/eu/vrtime/BootWicketWebApp/web/AbstractBasePage.java
@@ -4,6 +4,11 @@ import org.apache.wicket.markup.html.WebPage;
 
 public abstract class AbstractBasePage extends WebPage {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2244641676425667756L;
+
 	public AbstractBasePage() {
 		super();
 		add(new HeaderPanel("headerPanel"));
--- BootWicketWebApp/src/main/java/eu/vrtime/BootWicketWebApp/web/HomePage.java
+++ BootWicketWebApp/src/main/java/eu/vrtime/BootWicketWebApp/web/HomePage.java
@@ -2,11 +2,11 @@ package eu.vrtime.BootWicketWebApp.web;
 
 import org.apache.wicket.markup.html.basic.Label;
 
-public class HomePage extends AbstractBasePage{
+public class HomePage extends AbstractBasePage {
 	
 	public HomePage() {
 		super();
-		add(new Label("homePageLable", "this is the homePage"));
+		add(new Label("homePageLabel", "this is the homePage"));
 	}
 
 }
