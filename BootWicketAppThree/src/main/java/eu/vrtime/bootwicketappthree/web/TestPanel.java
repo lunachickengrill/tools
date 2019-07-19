d6a893cc357bea3b08645fdab39de45b48596ea8
tschwaiger <tschwaiger@libertyglobal.com>
2019-07-19 14:45:23 +0200

    added time/date label

--- BootWicketAppThree/src/main/java/eu/vrtime/bootwicketappthree/web/TestPanel.java
+++ BootWicketAppThree/src/main/java/eu/vrtime/bootwicketappthree/web/TestPanel.java
@@ -1,21 +1,29 @@
 package eu.vrtime.bootwicketappthree.web;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.apache.wicket.Component;
+import org.apache.wicket.IGenericComponent;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.util.SetModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -32,9 +40,14 @@ public class TestPanel extends Panel {
 	private static final String FORM_ID = "testForm";
 	private static final String DDC_ID = "type";
 	private static final String BUTTON_ID = "button";
-	
+	private static final String TIMELABEL_ID = "timeLabel";
+	private static final String REFRESHLINK_ID = "refreshLink";
+
 	private String type = new String();
 	private CompoundPropertyModel model;
+
+	private SimpleDateFormat dateFormat;
+	private String currentTime;
 
 	private FeedbackPanel feedback;
 
@@ -45,11 +58,33 @@ public class TestPanel extends Panel {
 
 	@Override
 	protected void onInitialize() {
-		// TODO Auto-generated method stub
 		super.onInitialize();
 		add(feedback);
 		add(createForm(FORM_ID));
+		
+		// adding a date/time display label that is refreshed via link
+		add(createDateLabel(TIMELABEL_ID));
+		add(createRefreshLink(REFRESHLINK_ID));
 
+	}
+
+	private Link<Void> createRefreshLink(final String id) {		
+		return new Link<Void>(id) {
+			private static final long serialVersionUID = 8796317657283576757L;
+			@Override
+			public void onClick() {
+				currentTime = dateFormat.format(new Date());
+			}		
+		};
+	}
+
+
+	
+	private Label createDateLabel(final String id) {
+		dateFormat = new SimpleDateFormat("hh:mm:ss");
+		currentTime = dateFormat.format(new Date());
+		PropertyModel model = new PropertyModel<>(this, "currentTime");
+		return new Label(id, model);
 	}
 
 	private Form createForm(final String id) {
@@ -68,14 +103,12 @@ public class TestPanel extends Panel {
 				super.onSubmit();
 				feedback.info(type);
 			}
-			
+
 		});
-		
 
 		return form;
 
 	}
-	
 
 	private Component createDDC(final String id) {
 		IModel<List<String>> model = new LoadableDetachableModel<List<String>>() {
