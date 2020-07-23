f1b877e2da97092f7217f7c94fd23f8bebf52a40
tschwaiger <tschwaiger@libertyglobal.com>
2020-07-23 14:27:37 +0200

    refactoring

--- src/main/java/eu/vrtime/jpa/FilterSpecification.java
+++ src/main/java/eu/vrtime/jpa/FilterSpecification.java
@@ -3,7 +3,7 @@ package eu.vrtime.jpa;
 import org.springframework.data.jpa.domain.Specification;
 
 /**
- * Interface for Specification classes. Must implemente method to build predicate. Can be used by AbstractEntityDataProvider implementations.
+ * Interface for Specification classes. Must implement method to build predicate. Can be used by AbstractEntityDataProvider implementations.
  * @author tschwaiger
  *
  * @param <T>
